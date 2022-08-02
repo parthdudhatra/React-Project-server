ff432aa524f407cc52ba46a07cce6cb25c957052
parth <parth.dudhatra@aspiresoftserv.in>
2022-08-02 18:31:29 +0530

    impliment the redux

--- controllers/users.js
+++ controllers/users.js
@@ -1,31 +1,79 @@
-const uuid = require('uuid').v4
+const uuid = require('uuid').v4;
+const User =  require("../models/user")
 
 let users = [];
 
+//Get all user
 exports.getUsers = (req, res, next) => {
-    res.send(users)
+    User.find((err, data) => {
+        if(err) {
+            return next(err);
+        }
+        else {
+            res.json(data)
+        }
+    })
 }
 
+// Add the user
 exports.createUser = (req, res, next) => {
-    const user = req.body
-    users.push({...user, id : uuid()})
-    res.send("User add successfully")
+    const user = User
+    User.create(req.body, (err, data) =>{
+        if (err) {
+            return next(err);
+        } else {
+            res.json(data); 
+        }
+    })  
 }
 
+//get one user
 exports.getUserById = (req, res ) => {
-    const singalUser = users.filter((user) => user.id === req.params.id); 
-    res.send(singalUser)
+    User.findById(req.params.id).then((data) => {
+        if (data) {
+          res.status(200).json(data);
+        } else {
+          res.status(404).json({ message: "Post not found!" });
+        }
+      });
+    // const singalUser = users.filter((user) => user.id === req.params.id); 
+    // res.send(singalUser)
 }
 
+// delete user
 exports.deleteUser = (req, res ) => {
-    users = users.filter((user) => user.id !== req.params.id);
-    res.send("User delete succefully")
+    User.findByIdAndRemove(req.params.id, (err, data) => {
+        if (err) {
+          return next(error);
+        } else {
+          res.status(200).json({
+            msg: data,
+          });
+        }
+      });
+    // users = users.filter((user) => user.id !== req.params.id);
+    // res.send("User delete succefully")
 }
 
+//update user
 exports.updateUser = (req, res ) => {
-    const user = users.find((user) => user.id === req.params.id);
-    user.name = req.body.name;
-    user.email = req.body.email;
-    user.contact = req.body.contact
-    res.send("User update succefully")
+    User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        (error, data) => {
+          if (error) {
+            console.log(error);
+          } else {
+            res.json(data);
+            console.log("Employee updated successfully");
+          }
+        }
+      );
+    // const user = users.find((user) => user.id === req.params.id);
+    // user.name = req.body.name;
+    // user.email = req.body.email;
+    // user.contact = req.body.contact
+    // res.send("User update succefully")
 }
--- index.js
+++ index.js
@@ -1,25 +1,41 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser')
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-// import userRoutes from "./routes/user"
-const authrouter = require('./routes/auth-route');
+
 
 const app = express();
 const port = process.env.PORT || 3000;
+// import userRoutes from "./routes/user"
+const authrouter = require("./routes/auth-route");
+const mongoose = require("mongoose");
+
+app.use(
+  express.urlencoded({ extended: true })
+);
+  
+app.use(express.json());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("Hello From Express")
-})
+//monoose
+mongoose.connect(
+  "mongodb+srv://parth:SBT6aWXVV9r1Eqpw@cluster0.yx8i0pv.mongodb.net/test",
+  (err) => {
+    if (err) {
+      console.log("Db is not connecting");
+    } else {
+      console.log("Db is connecting");
+    }
+  }
+);
 
-app.use('/auth', authrouter)
+app.use("/auth", authrouter);
 
-app.all('*',(req, res) => res.send("That route doent's exits"))
+app.all("*", (req, res) => res.send("That route doent's exits"));
 
 app.listen(port, () => {
-    console.log("server is connected", port)
-})
+  console.log("server is connected", port);
+});
